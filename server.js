fbe62174226ee56528d8d71bb5dfb2215d62451b
Ubuntu <mwadmin@DockerServer.acc1dry2itkuvigf4gnzcy3wzb.cx.internal.cloudapp.net>
2018-10-09 09:04:16 +0000

    changes to config

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Nodejs Web-app\n');
+  res.send('Hello world !!! Nodejs webApp \n');
 });
 
 app.listen(PORT, HOST);
